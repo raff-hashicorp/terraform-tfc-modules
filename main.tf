ef403ae7f3c4980255442679aa4a7fd1465f3d21
raff-hashicorp <84794994+raff-hashicorp@users.noreply.github.com>
2022-03-09 16:19:37 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  cloud {
+    organization = "bns-terraform-workshop-rs"
+
+    workspaces {
+      name = "proj-sb-core-workspace-creator"
+    }
+  }
+}
+
+
+provider "tfe" {
+  hostname = var.hostname # Update tf variables in TFCB to app.terraform.io
+  token    = var.tfcb_token # Enter you TFCB Token. Mark as sensitive
+  version  = "~> 0.27.0"
+}
+
+# Create an organization, Not required for this exercise
+#resource "tfe_organization" "org" {
+  # ...
+#}
+
+## Waiting on Feature Request to be able to build Variable Sets
+
+# Create the core set of workspaces
+resource "tfe_workspace" "test" {
+  name         = var.workspace_name # Update tf variables in TFCB to our workshop environment
+  organization = var.tfcb_org # Update tf variables in TFCB to our workshop environment
+  tag_names    = var.tag_name # Update tf variables in TFCB to our workshop environment
+}
