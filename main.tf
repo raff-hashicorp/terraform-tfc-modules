cb7864d7981c8f1189ab6a9241442773ba0325c7
raff-hashicorp <84794994+raff-hashicorp@users.noreply.github.com>
2022-03-09 21:00:51 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,18 +7,12 @@ terraform {
     }
   }
   
-  terraform {
   required_providers {
     tfe = {
       source = "hashicorp/tfe"
       version = "0.28.1"
     }
   }
-}
-
-provider "tfe" {
-  # Configuration options
-}
 }
 
 
