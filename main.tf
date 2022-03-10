3bf6e47ce39250cbc6cce2ba1cd30682bccacb69
raff-hashicorp <84794994+raff-hashicorp@users.noreply.github.com>
2022-03-09 21:40:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ provider "tfe" {
 # Create the core set of workspaces
 resource "tfe_workspace" "proj-sb-appid-workspace" {
   for_each = toset(var.workspace_names)
-    name = each.value
+    name = "${var.appid}-${each.value}"
  
   organization = var.tfcb_org # Update tf variables in TFCB to our workshop environment
 }
