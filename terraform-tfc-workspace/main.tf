a7aef3ac7718f1ccf0a9d1c884929371313f5496
raff-hashicorp <84794994+raff-hashicorp@users.noreply.github.com>
2022-03-10 08:46:09 -0500

    Update main.tf

--- terraform-tfc-workspace/main.tf
+++ terraform-tfc-workspace/main.tf
@@ -1,13 +1,4 @@
-
 terraform {
-  cloud {
-    organization = "bns-terraform-workshop-rs"
-
-    workspaces {
-      name = "proj-sb-core-workspace-creator"
-    }
-  }
-  
   required_providers {
     tfe = {
       source = "hashicorp/tfe"
@@ -16,23 +7,13 @@ terraform {
   }
 }
 
-
 provider "tfe" {
-  hostname = var.hostname # Update tf variables in TFCB to app.terraform.io
 }
-
-# Create an organization, Not required for this exercise
-#resource "tfe_organization" "org" {
-  # ...
-#}
-
-## Waiting on Feature Request to be able to build Variable Sets
 
 # Create the core set of workspaces
 resource "tfe_workspace" "proj-sb-appid-workspace" {
   for_each = toset(var.workspace_names)
     name = "${var.prefix}-${var.appid}-${each.value}"
- 
-  organization = var.tfcb_org # Update tf variables in TFCB to our workshop environment
+   organization = var.tfcb_org # Update tf variables in TFCB to our workshop environment
 }
 
