b859114371e180cbd0ef86bb55a4f41363463015
raff-hashicorp <84794994+raff-hashicorp@users.noreply.github.com>
2022-03-09 20:43:54 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "tfcb_org" {
     description = "The TFCB/TFE Org Name"
     default     = "bns-terraform-workshop-rs"
 }
+variable "hostname" {
+    description = "tfc/tfcb hostname"
+}
+variable "tfcb_token" {
+    description = "tfcb token"
+}
