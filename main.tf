c3da20ed60d286b59dbb8e660e55c4f48812a676
raff-hashicorp <84794994+raff-hashicorp@users.noreply.github.com>
2022-03-09 20:55:58 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-locals {
-  workspace_names = ["bob", "kevin", "stewart"]
-}
-
 terraform {
   cloud {
     organization = "bns-terraform-workshop-rs"
@@ -10,13 +6,25 @@ terraform {
       name = "proj-sb-core-workspace-creator"
     }
   }
+  
+  terraform {
+  required_providers {
+    tfe = {
+      source = "hashicorp/tfe"
+      version = "0.28.1"
+    }
+  }
+}
+
+provider "tfe" {
+  # Configuration options
+}
 }
 
 
 provider "tfe" {
   hostname = var.hostname # Update tf variables in TFCB to app.terraform.io
   token    = var.tfcb_token # Enter you TFCB Token. Mark as sensitive
-  version  = "~> 0.28.1"
 }
 
 # Create an organization, Not required for this exercise
@@ -28,7 +36,7 @@ provider "tfe" {
 
 # Create the core set of workspaces
 resource "tfe_workspace" "proj-sb-appid-workspace" {
-  for_each = toset(local.workspace_names)
+  for_each = toset(var.workspace_names)
     name = each.value
  
   organization = var.tfcb_org # Update tf variables in TFCB to our workshop environment
